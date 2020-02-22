1d2c00aaaaf41acec5b48ec030c7fe24eba5dd42
Petar Ostojic <pera.ostojic@gmail.com>
2020-02-23 00:45:40 +0100

    changed mongodb uri variable

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URL, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true
 });
 const db = mongoose.connection;
