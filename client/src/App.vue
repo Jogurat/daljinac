733b3e18e0528004cae2c9b598020983ce36d7b4
Petar Ostojic <pera.ostojic@gmail.com>
2020-03-01 19:20:10 +0100

    Added client files

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <router-view />
+    <HelloWorld />
+  </v-app>
+</template>
+
+<script>
+import Vue from "vue";
+//import VueRouter from "vue-router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import HelloWorld from "./components/HelloWorld";
+library.add(faUserSecret);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+export default {
+  name: "App",
+
+  components: {
+    HelloWorld
+  },
+
+  data: () => ({
+    //
+  })
+};
+</script>
---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <v-card class="mx-auto" max-width="344">
+      <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+
+      <v-card-title>Top western road trips</v-card-title>
+
+      <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
+
+      <v-card-actions>
+        <v-btn text>Share</v-btn>
+
+        <v-btn color="purple" text>Explore</v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon @click="show = !show">
+          <v-icon>{{ show ? "mdi-chevron-up" : "mdi-chevron-down" }}</v-icon>
+        </v-btn>
+      </v-card-actions>
+
+      <v-expand-transition>
+        <div v-show="show">
+          <v-divider></v-divider>
+          <v-btn icon>
+            <v-icon>mdi-power</v-icon>
+          </v-btn>
+        </div>
+      </v-expand-transition>
+    </v-card>
+  </div>
+</template>
+
+<script>
+//import Login from "./Login";
+//import Register from "./Register";
+
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    show: false
+  }),
+  components: {
+    //Register
+  }
+};
+</script>
