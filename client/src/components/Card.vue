93c2e57d3d43f57c3f681ee1fd22493137cabf79
Petar Ostojic <pera.ostojic@gmail.com>
2020-03-28 15:31:18 +0100

    Changed icon and page title, wrapped v-card in Card component

---
+++ client/src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <v-card>
+      <v-card-title>{{ $props.title }}</v-card-title>
+      <v-card-text>{{ $props.deviceID }}</v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+  data: function() {
+    return {
+      //title: ""
+    };
+  },
+  props: {
+    title: String,
+    deviceID: Number
+  }
+};
+</script>
+
+<style>
+</style>
