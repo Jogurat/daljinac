c159afcc4d17798a7572b3701f170ea5e2f89ab2
Petar Ostojic <pera.ostojic@gmail.com>
2020-03-29 20:27:12 +0200

    Changed docs and prototype according to formal inspection of team Nebula

--- client/src/components/Card.vue
+++ client/src/components/Card.vue
@@ -3,6 +3,9 @@
     <v-card>
       <v-card-title>{{ $props.title }}</v-card-title>
       <v-card-text>{{ $props.deviceID }}</v-card-text>
+      <v-btn icon>
+        <v-icon>mdi-alpha-r-circle-outline</v-icon>
+      </v-btn>
     </v-card>
   </div>
 </template>
