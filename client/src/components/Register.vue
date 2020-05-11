5b4635dbdd930a780b17efcd2f45aed28eee6091
Petar Ostojic <pera.ostojic@gmail.com>
2020-05-11 12:47:13 +0200

    Debugging config file in client

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,52 +1,57 @@
 <template>
-  
-    <v-dialog  v-model="dialog"  overlay-opacity="0.75" width="400px">
-      
-      <template v-slot:activator="{ on }">
-        <v-btn class="ma-4" color="blue accent-3" dark v-on="on">Register</v-btn>
-      </template>
-      <v-form
-      ref="form"
-      v-model="valid"
-      :lazy-validation="lazy"
-    >
+  <v-dialog v-model="dialog" overlay-opacity="0.75" width="400px">
+    <template v-slot:activator="{ on }">
+      <v-btn class="ma-4" color="blue accent-3" dark v-on="on">Register</v-btn>
+    </template>
+    <v-form ref="form" v-model="valid" :lazy-validation="lazy">
       <v-card>
         <v-card-title class="justify-center">
           <v-row>
             <v-col cols="10">
-          <span class="headline">Make user profile</span>
-          </v-col>
-          <v-col cols="2">
-          <v-btn  icon color="blue darken-1" v-on:click="close">
-            <v-icon medium>mdi-close</v-icon>
-          </v-btn>
-           </v-col>
+              <span class="headline">Make user profile</span>
+            </v-col>
+            <v-col cols="2">
+              <v-btn icon color="blue darken-1" v-on:click="close">
+                <v-icon medium>mdi-close</v-icon>
+              </v-btn>
+            </v-col>
           </v-row>
         </v-card-title>
         <v-card-text>
           <v-container>
             <v-row>
-              <v-col cols="12" >
-                <v-text-field label="Email*" v-model="email" :rules="emailRules" filled rounded dense required></v-text-field>
+              <v-col cols="12">
+                <v-text-field
+                  label="Email*"
+                  v-model="email"
+                  :rules="emailRules"
+                  filled
+                  rounded
+                  dense
+                  required
+                ></v-text-field>
               </v-col>
             </v-row>
             <v-row>
               <v-col cols="12">
-                <v-text-field 
-                  label="Username*" v-model="username" filled rounded dense
-                   required></v-text-field>
+                <v-text-field label="Username*" v-model="username" filled rounded dense required></v-text-field>
               </v-col>
             </v-row>
             <v-row>
               <v-col cols="12">
-                <v-text-field  label="Password*"  required filled rounded dense
-                v-model="password"
-              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-              :rules="[rules.required, rules.min]"
-              :type="show1 ? 'text' : 'password'"
-              hint="At least 5 characters"
-              counter
-             @click:append="show1 = !show1"
+                <v-text-field
+                  label="Password*"
+                  required
+                  filled
+                  rounded
+                  dense
+                  v-model="password"
+                  :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                  :rules="[rules.required, rules.min]"
+                  :type="show1 ? 'text' : 'password'"
+                  hint="At least 5 characters"
+                  counter
+                  @click:append="show1 = !show1"
                 ></v-text-field>
               </v-col>
             </v-row>
@@ -58,23 +63,14 @@
           <!--<v-btn icon color="blue darken-1" text v-on:click="reset">
             <v-icon medium>mdi-restore</v-icon>
           </v-btn>-->
-          
 
-          
-          <v-btn  color="blue darken-1" text v-on:click="registerUser">
-            Register</v-btn>
-            
-          
+          <v-btn color="blue darken-1" text v-on:click="registerUser">Register</v-btn>
         </v-card-actions>
       </v-card>
-      
-     </v-form>
-     
-    </v-dialog>
-    
-    
+    </v-form>
+  </v-dialog>
 
-    <!--<v-container>
+  <!--<v-container>
       <v-row>
         <h1>Register</h1>
       </v-row>
@@ -98,9 +94,7 @@
           <v-btn v-on:click="registerUser">Register</v-btn>
         </v-col>
       </v-row>
-    </v-container>-->
-
- 
+  </v-container>-->
 </template>
 
 <script>
@@ -112,26 +106,27 @@ console.log(url);
 export default {
   name: "Register",
   data: () => ({
-      username: "",
-      password: "",
-      //email: "",
-      dialog:false,
-      valid: true,
-      show1: false,
-       rules: {
-          required: value => !!value || 'Required.',
-          min: v => v.length >= 5 || 'Min 5 characters',
-        },
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-      ],
-      lazy: false
-    }),
+    username: "",
+    password: "",
+    //email: "",
+    dialog: false,
+    valid: true,
+    show1: false,
+    rules: {
+      required: value => !!value || "Required.",
+      min: v => v.length >= 5 || "Min 5 characters"
+    },
+    email: "",
+    emailRules: [
+      v => !!v || "E-mail is required",
+      v => /.+@.+\..+/.test(v) || "E-mail must be valid"
+    ],
+    lazy: false
+  }),
   methods: {
     registerUser: function() {
       console.log(url);
+      console.log(config);
       axios.post(`${url}/users`, {
         username: this.username,
         password: this.password,
@@ -141,19 +136,19 @@ export default {
     /*validate () {
         this.$refs.form.validate()
       },*/
-      /*reset () {
+    /*reset () {
         this.$refs.form.reset();
       },*/
-      close(){
-        this.$refs.form.reset();
-        this.dialog=false;
-      },
+    close() {
+      this.$refs.form.reset();
+      this.dialog = false;
+    }
   }
 };
 </script>
 
 <style scoped>
-.headline{
+.headline {
   align-content: center;
   text-align: center;
 }
