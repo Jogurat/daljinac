6e05b93ff094b2850e8a69318365b83913543ef8
Petar Ostojic <pera.ostojic@gmail.com>
2020-05-11 12:32:54 +0200

    Changed config file for client

--- client/config.js
+++ client/config.js
@@ -1,10 +1,10 @@
 let config = {};
 
-config.PORT = process.env.PORT || 3000;
-config.SECRET_KEY = process.env.SECRET_KEY || "secret_key";
-config.EMAIL_SECRET = process.env.EMAIL_SECRET || "secret_email";
-config.DB_HOST = process.env.DB_HOST || "http://localhost";
-config.FRONT_HOST = process.env.FRONT_HOST || "http://localhost";
+config.PORT = process.env.VUE_APP_PORT || 3000;
+config.SECRET_KEY = process.env.VUE_APP_SECRET_KEY || "secret_key";
+config.EMAIL_SECRET = process.env.VUE_APP_EMAIL_SECRET || "secret_email";
+config.DB_HOST = process.env.VUE_APP_DB_HOST || "http://localhost";
+config.FRONT_HOST = process.env.VUE_APP_FRONT_HOST || "http://localhost";
 
 if (process.env.NODE_ENV == "development")
   config.MONGODB_URI = "mongodb://localhost/actions";
