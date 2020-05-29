a2215c5274a2939e4525e92d3fca78b689cc2edc
Petar Ostojic <pera.ostojic@gmail.com>
2020-05-29 19:02:30 +0200

    Automatic update of isProceseed flag in actionController

--- controllers/actionController.js
+++ controllers/actionController.js
@@ -45,6 +45,8 @@ actionController.getFirstUnprocessed = async (req, res) => {
     const last = actions.sort((a, b) => {
       b.createdAt - a.createdAt;
     });
+    last[0].isProcessed = true;
+    await last[0].save();
     res.status(200).json(last[0].bits); // Only bits or entire obj?
   } catch (err) {
     res.status(500).json({ message: err.message });
