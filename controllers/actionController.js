48b444c03ed6cfc08c90917c5e393c9eac6fb832
Petar Ostojic <pera.ostojic@gmail.com>
2020-06-01 21:02:50 +0200

    Big overhaul of README

--- controllers/actionController.js
+++ controllers/actionController.js
@@ -71,6 +71,7 @@ actionController.getFirstUnprocessed = async (req, res) => {
     res.status(200).json({ bits: last[0].bits }); // Only bits or entire obj?
   } catch (err) {
     res.status(500).json({ message: err.message });
+    console.log(err);
   }
 };
 
